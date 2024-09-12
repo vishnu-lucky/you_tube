7889ffc984bbbdec0526fa5c1ef85fda29fde632
vishnu-lucky <vishnulucky94414@gmail.com>
2024-09-12 13:35:33 +0530

    adjustment

--- youtube/src/components/Header.js
+++ youtube/src/components/Header.js
@@ -8,8 +8,9 @@ function Header() {
     <div className="header">
       <img className="header__logo" src="https://tse3.mm.bing.net/th?id=OIP.DYbaRGUNYuDG7ivK9jm9uQHaEK&pid=Api&P=0&h=180" alt="YouTube" />
       <input className="header__searchInput" type="text" placeholder="Search" />
-      <button className="header__searchButton">🔍</button>
       <div className="header__icons">
+      <button>🔍</button>
+     
         <button>🔔</button>
         <button>📷</button>
         <button>👤</button>
--- youtube/src/components/VideoFeed.js
+++ youtube/src/components/VideoFeed.js
@@ -1,28 +1,33 @@
 // VideoFeed.js
 import React from 'react';
 
-
-
 function VideoFeed() {
   const videos = [
     { 
-      title: 'Understanding Algorithms', 
-      channel: 'Tech Guru', 
-      thumbnail: '', 
+      title: 'SIDEMEN AMONG US PROXIMITY CHAT BUT EVERYONE S A JESTER', 
+      channel: 'SIDE MAN', 
+      thumbnail: 'yt2.png',  // Name of the image file
     },
     { 
-      title: 'React for Beginners', 
-      channel: 'Dev Academy', 
-      thumbnail: '', 
+      title: 'AUS VS ENG', 
+      channel: 'ICC', 
+      thumbnail: 'yt3.png',  // Name of the second image file
     },
   ];
-  
 
   return (
     <div className="videoFeed">
       {videos.map((video, index) => (
-        <div key={index} className="videoCard">
-          <img src={video.thumbnail} alt={video.title} />
+        <div key={index} className="videoCard" style={{ width: '300px', height: '200px' }}>
+          <img 
+            src={require(`../img/${video.thumbnail}`)}  // Dynamically require the image
+            alt={video.title} 
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover'
+            }}
+          />
           <div className="videoInfo">
             <h4>{video.title}</h4>
             <p>{video.channel}</p>
